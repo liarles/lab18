8bb2e5207945c047f8de3860e1cf4d15a9a45953
liarles <62631264+liarles@users.noreply.github.com>
2025-02-08 16:06:49 +0700

    Update lab18_1.cpp

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,12 +1,21 @@
 #include<iostream>
 using namespace std;
 
-struct Rect{
-	double x,y,w,h;
+struct Rect {
+    double x, y, w, h;
 };
 
-double overlap(){
+double overlap(Rect R1, Rect R2) {
+    Rect lap;
+    
+    lap.x = max(R1.x, R2.x);
+    lap.y = min(R1.y, R2.y);
+    lap.w = min(R1.x + R1.w, R2.x + R2.w) - lap.x;
+    lap.h = lap.y - max(R1.y - R1.h, R2.y - R2.h);
 
+    if (lap.w <= 0 || lap.h <= 0) {
+        return 0;
+    }
 
-
+    return lap.w * lap.h;
 }
